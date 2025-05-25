7e9cec2d53d74beeaa050b46178cbd563400a124
Ahmad Elkholy <ahmadkholy98@gmail.com>
2025-05-25 17:55:31 +0300

    init frontend & install tailwind css

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+
+    <div class="dark:bg-slate-950 bg-slate-700">
+      <nav class="container mx-auto flex items-center justify-between p-4">
+        <RouterLink class="text-slate-200" to="/">Home</RouterLink>
+        <div class="flex space-x-2">
+          <RouterLink class="text-slate-200" to="/login">Login</RouterLink>
+          <RouterLink class="text-slate-200" to="/register">Register</RouterLink>
+        </div>
+      </nav>
+    </div>
+  </header>
+
+  <main class="max-w-6xl mx-auto bg-slate-100 dark:bg-slate-900 min-h-screen">
+    <div class="mb-6">
+      <RouterView />
+    </div>
+  </main>
+</template>
+
---
+++ frontend/src/views/auth/Dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1> Dashboard</h1>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
